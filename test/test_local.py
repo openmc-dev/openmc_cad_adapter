aacb8f6b9a7fa04e7b313667d4b620a0104b884a
Patrick Shriwise <pshriwise@gmail.com>
2024-09-30 20:15:57 -0500

    Adding test for cylinder

--- test/test_local.py
+++ test/test_local.py
@@ -6,6 +6,9 @@ from openmc_cad_adapter import to_cubit_journal
 from .test_utilities import diff_gold_file
 
 
+# Test the XCylinder and YCylinder classes, the ZCylinder surface is tested
+# extensively in the OpenMC example tests
+
 def test_xcylinder(request):
     x_cyl = openmc.XCylinder(r=1.0, y0=10.0, z0=5.0)
     g = openmc.Geometry([openmc.Cell(region=-x_cyl)])
@@ -19,4 +22,8 @@ def test_ycylinder(request):
     to_cubit_journal(g, world=(500, 500, 500), filename='ycylinder.jou')
     diff_gold_file('ycylinder.jou')
 
-
+def test_cylinder(request):
+    cyl = openmc.Cylinder(x0=0.0, y0=0.0, z0=0.0, r=6.0, dx=0.7071, dy=0.7071, dz=0.0)
+    g = openmc.Geometry([openmc.Cell(region=-cyl)])
+    to_cubit_journal(g, world=(500, 500, 500), filename='cylinder.jou')
+    diff_gold_file('cylinder.jou')
