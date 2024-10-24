d11cff8ef0bd76a4559906b3e365a94bf3bc75c5
Patrick Shriwise <pshriwise@gmail.com>
2024-10-24 10:54:24 -0500

    Adding axis-aligned cone tests

--- test/test_local.py
+++ test/test_local.py
@@ -27,3 +27,21 @@ def test_cylinder(request):
     g = openmc.Geometry([openmc.Cell(region=-cyl)])
     to_cubit_journal(g, world=(500, 500, 500), filename='cylinder.jou')
     diff_gold_file('cylinder.jou')
+
+def test_x_cone(request):
+    x_cone = openmc.XCone(x0=30.0, y0=3.0, z0=5.0, r2=5.0)
+    g = openmc.Geometry([openmc.Cell(region=-x_cone)])
+    to_cubit_journal(g, world=(500, 500, 500), filename='x_cone.jou')
+    diff_gold_file('x_cone.jou')
+
+def test_y_cone(request):
+    y_cone = openmc.YCone(x0=40.0, y0=20.0, z0=7.0, r2=2.0)
+    g = openmc.Geometry([openmc.Cell(region=-y_cone)])
+    to_cubit_journal(g, world=(500, 500, 500), filename='y_cone.jou')
+    diff_gold_file('y_cone.jou')
+
+def test_z_cone(request):
+    z_cone = openmc.ZCone(x0=50.0, y0=10.0, z0=2.0, r2=1.0)
+    g = openmc.Geometry([openmc.Cell(region=-z_cone)])
+    to_cubit_journal(g, world=(500, 500, 500), filename='z_cone.jou')
+    diff_gold_file('z_cone.jou')
