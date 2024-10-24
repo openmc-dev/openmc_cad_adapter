08551ac5bfdddffa71aabbb42aa2d7482e5beb12
Patrick Shriwise <pshriwise@gmail.com>
2024-10-24 11:08:18 -0500

    p8

--- test/test_local.py
+++ test/test_local.py
@@ -22,11 +22,13 @@ def test_ycylinder(request):
     to_cubit_journal(g, world=(500, 500, 500), filename='ycylinder.jou')
     diff_gold_file('ycylinder.jou')
 
+
 def test_cylinder(request):
     cyl = openmc.Cylinder(x0=0.0, y0=0.0, z0=0.0, r=6.0, dx=0.7071, dy=0.7071, dz=0.0)
     g = openmc.Geometry([openmc.Cell(region=-cyl)])
     to_cubit_journal(g, world=(500, 500, 500), filename='cylinder.jou')
     diff_gold_file('cylinder.jou')
+
 
 def test_x_cone(request):
     x_cone = openmc.XCone(x0=30.0, y0=3.0, z0=5.0, r2=5.0)
@@ -34,11 +36,13 @@ def test_x_cone(request):
     to_cubit_journal(g, world=(500, 500, 500), filename='x_cone.jou')
     diff_gold_file('x_cone.jou')
 
+
 def test_y_cone(request):
     y_cone = openmc.YCone(x0=40.0, y0=20.0, z0=7.0, r2=2.0)
     g = openmc.Geometry([openmc.Cell(region=-y_cone)])
     to_cubit_journal(g, world=(500, 500, 500), filename='y_cone.jou')
     diff_gold_file('y_cone.jou')
+
 
 def test_z_cone(request):
     z_cone = openmc.ZCone(x0=50.0, y0=10.0, z0=2.0, r2=1.0)
